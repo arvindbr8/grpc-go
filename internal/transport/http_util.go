6d04b9e2bf1992e46a3ff15ad9dd5147c20b5531
Arvind Bright <arvind.bright100@gmail.com>
2023-10-02 19:32:49 -0700

    change

--- internal/transport/http_util.go
+++ internal/transport/http_util.go
@@ -163,6 +163,13 @@ func decodeGRPCStatusDetails(rawDetails string) (*status.Status, error) {
 	if err = proto.Unmarshal(v, st); err != nil {
 		return nil, err
 	}
+	print("hello")
+	print("hello")
+	print("hello")
+	print("hello")
+	print("hello")
+	print("hello")
+	print("hello")
 	return status.FromProto(st), nil
 }
 
